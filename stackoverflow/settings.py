c765171f5a0cd1c8f5ec7668209c907e4809363a
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-03-30 13:44:27 +0500

    Update for deploy

--- stackoverflow/settings.py
+++ stackoverflow/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,15 +55,17 @@ MIDDLEWARE = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'HOST': os.getenv('DB_HOST'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'PORT': os.getenv('DB_PORT')
-    }
+    'default': dj_database_url.config(
+        default='postgresql://{user}:{password}@{host}:{port}/{name}'.format(
+            user=os.getenv('DB_USER', 'postgres'),
+            password=os.getenv('DB_PASSWORD', '123654'),
+            host=os.getenv('DB_HOST', 'localhost'),
+            port=os.getenv('DB_PORT', '5432'),
+            name=os.getenv('DB_NAME', 'stackoverflowDB')
+        )
+    )
 }
+
 
 
 ROOT_URLCONF = 'stackoverflow.urls'
